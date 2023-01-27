e9ba666c0ac40d26ac72778bbaa5294e6eb83085
IShaLinI <mrsaltinecracker1@gmail.com>
2023-01-27 09:08:29 -0800

    popup

--- index.js
+++ index.js
@@ -1,3 +1,5 @@
+const LoginPopupLoginButton = document.getElementById("btnLogin");
+
 
 function showLoginPrompt(){
     document.getElementById("LoginPrompt").style.opacity = 1;
@@ -7,3 +9,7 @@ function hideLoginPrompt(){
     document.getElementById("LoginPrompt").style.opacity = 0;
     document.getElementById("LoginForm").reset();
 }
+
+LoginPopupLoginButton.addEventListener("click", function(){
+    
+});
