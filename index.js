e0837bc93ed3099fce534b0feb92ca940bcbc992
IShaLinI <mrsaltinecracker1@gmail.com>
2023-01-27 09:58:36 -0800

    Remember me button works now

--- index.js
+++ index.js
@@ -1,8 +1,13 @@
-const LoginPopupLoginButton = document.getElementById("btnLogin");
-
-
 function showLoginPrompt(){
     document.getElementById("LoginPrompt").style.opacity = 1;
+
+    document.getElementById("username").value = localStorage.getItem("username");
+    document.getElementById("password").value = localStorage.getItem("password");
+
+    if(localStorage.getItem("remember") == "true"){
+        document.getElementById("rememberMe").checked = true;
+    }
+
 }
 
 function hideLoginPrompt(){
@@ -10,6 +15,16 @@ function hideLoginPrompt(){
     document.getElementById("LoginForm").reset();
 }
 
-LoginPopupLoginButton.addEventListener("click", function(){
-    
-});
+function login(){
+
+    if(document.getElementById("rememberMe").checked){
+        localStorage.setItem("username", document.getElementById("username").value);
+        localStorage.setItem("password", document.getElementById("password").value);
+    }else{
+        localStorage.setItem("username", "");
+        localStorage.setItem("password", "");
+    }
+
+    localStorage.setItem("remember", document.getElementById("rememberMe").checked);
+
+}
